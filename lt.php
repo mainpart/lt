fc4119df531c9c9638322ae8ae778635b450df03
Дмитрий Красников <dykrasni@mts.ru>
2021-11-26 14:26:58 +0300

    смена версии

--- lt.php
+++ lt.php
@@ -8,7 +8,7 @@
    GitHub Plugin URI: https://github.com/mainpart/lt
    Primary Branch: main
    Domain Path: /languages
-   Version: 1.0.2
+   Version: 1.0.3
    Description: Плагин для организации консультаций
 */
 
