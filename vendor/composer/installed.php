166063be27a588e8562f78d10c9c57e32e070f0e
Дмитрий Красников <dmitriy.krasnikov@sbermarket.ru>
2023-05-14 10:41:10 +0300

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '472b0c4a1eb33b11dfb424b2660374a6bce5ad4d',
+        'name' => 'dykrasni/lt',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'afragen/wp-dependency-installer' => array(
+            'pretty_version' => '4.2.2',
+            'version' => '4.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../afragen/wp-dependency-installer',
+            'aliases' => array(),
+            'reference' => '5e5419348c6cde7b02f3c33fb3fc3381062443a1',
+            'dev_requirement' => false,
+        ),
+        'afragen/wp-dismiss-notice' => array(
+            'pretty_version' => '0.2.1',
+            'version' => '0.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../afragen/wp-dismiss-notice',
+            'aliases' => array(),
+            'reference' => 'a0ced9836671d1374ed5d7f47e63ca313f5f54b9',
+            'dev_requirement' => false,
+        ),
+        'dykrasni/lt' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '472b0c4a1eb33b11dfb424b2660374a6bce5ad4d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
